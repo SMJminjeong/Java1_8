3e5897a5c2fbee6757447f6983b8480cf81de0fa
SMJminjeong <sdwp0920@gmail.com>
2022-09-14 13:19:05 +0900

    Section [1-3] 진행중

--- src/main/java/com/example/java1_8/Foo.java
+++ src/main/java/com/example/java1_8/Foo.java
@@ -1,19 +1,27 @@
 package com.example.java1_8;
 
 
-//8이전에는 인터페이스의 구현체를 만들어서 사용
-public class Foo {
-    public static void main(String[] args) {
-        //final 이라고 가정하고 사용하는 것
-//        int baseNumber = 10;
+import java.util.function.Function;
 
-        RunSomething runSomething = new RunSomething() {
-            int baseNumber = 10;
-            @Override
-            public int doIt(int number) {
-                return number + baseNumber;
-            }
-        };
+public class Foo {
+//    public static void main(String[] args) {
+//        Plus10 plus10 = new Plus10();
+//        System.out.println(plus10.apply(1));
+//
+//    }
+    public static void main(String[] args) {
+        Function<Integer, Integer> plus10 = (number) -> number+10;
+        Function<Integer, Integer> multiply2 = (number) -> number*2;
+        System.out.println(multiply2.apply(1));
+
+        //두 함수 조합
+        Function<Integer, Integer> multiply2AndPlus10 = plus10.compose(multiply2);
+        System.out.println(multiply2AndPlus10.apply(2)); //14
+        //compose가 입력값을 가지고 뒤에 오는 함수에 적용
+        //결과값을 plus10의 입력값으로 적용
+        //값 -> *2 -> *2의 값 -> +10 -> 결과값
+
+        plus10.andThen(multiply2);
 
     }
 }
